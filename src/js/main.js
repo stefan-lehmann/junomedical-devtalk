4fbac778098ac24d13839e9a4c001b3fd083568f
stefan-lehmann <stefan@lehmann.st>
2016-04-21 02:19:41 +0200

    wip

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,39 @@
 Reveal.initialize({
-	controls: false,
-	progress: true,
-	history: true,
-	center: false,
+  controls: false,
+  progress: true,
+  history: true,
+  center: false,
   overview: true,
   slideNumber: 'c/t',
 
-	transition: 'none', // none/fade/slide/convex/concave/zoom
+  transition: 'none', // none/fade/slide/convex/concave/zoom
 
-	// Optional reveal.js plugins
-	dependencies: [
-		{ src: '/js/vendor/classList.js', condition: function() { return !document.body.classList; } },
-		{ src: 'js/vendor/highlight/highlight.js', async: true, condition: function() { return !!document.querySelector( 'pre code' ); }, callback: function() { hljs.initHighlightingOnLoad(); } },
-		{ src: 'js/vendor/notes/notes.js', async: true }
-	]
+  // Optional reveal.js plugins
+  dependencies: [
+    {
+      src: '/js/vendor/classList.js', condition: function () {
+      return !document.body.classList;
+    }
+    },
+    {
+      src: 'js/vendor/highlight/highlight.js',
+      async: true,
+      condition: function () {
+        return !!document.querySelector('pre code');
+      },
+      callback: function () {
+        hljs.initHighlightingOnLoad();
+      }
+    },
+    {src: 'js/vendor/notes/notes.js', async: true}
+  ]
 });
 
 
 WebFontConfig = {
-  google: { families: [ 'Open+Sans:400,600,700,300:latin' ] }
+  google: {families: ['Open+Sans:400,600,700,300:latin']}
 };
-(function() {
+(function () {
   var wf = document.createElement('script');
   wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
   wf.type = 'text/javascript';
